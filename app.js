f91a235ce7d8d00313eefa79f19cb792261474bb
sothatt <147292652+sothatt@users.noreply.github.com>
2026-06-29 14:30:35 +0700

    push

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.json());
-require('dotenv').config();
+const port = 3000;
 
-const auth = require('./routes/auth');
+app.get('/',(req,res)=>{
+    console.log(req.query);
+    console.log("Hello world");
 
-app.use('/api/',auth);
+    res.send("<h1>Job Finder API</h1><p>Welcome to Job Finder API</p>")
+    
+})
+app.post('/register/:id', (req, res) => {
+    console.log("Hello world");
+    console.log(req.params.id); // Access the ID parameter
+    res.send("<h1>Job Finder API</h1><p>Welcome to Job Finder API</p>");
+});
 
-app.listen(3001, ()=>{
-    console.log("server runing on port 3001");
+app.listen(port,()=>{
+    console.log(`Server running on http://localhost:${port}`);
     
 })
